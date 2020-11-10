d9191b6387a19daf2ff4e9c93f3c780a438addd9
Nina Dissard <dissardnina@gmail.com>
2020-11-10 15:36:32 +0100

    Create a new method with a form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,21 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.create(task_params)
+    redirect_to task_path(task)
+  end
+
   private
+
+  # Strong Params
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 
   def set_task
     @task = Task.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # route to list the tasks
   get "tasks", to: "tasks#index"
+  # Create a new task
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
   get "tasks/:id", to: "tasks#show", as: "task"
+
 end
